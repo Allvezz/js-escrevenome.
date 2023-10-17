1f6951e90862a42371f51d7a2f97c0e50efc60e5
Allvezz <145471842+Allvezz@users.noreply.github.com>
2023-10-17 11:40:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+  createCanvas(400, 400);
+   background("red");
+}
+
+function draw() {
+  stroke("blue")
+  fill("white")
+ 
+  if(mouseIsPressed)
+  rect(mouseX, mouseY, 20, 20);
+}   
